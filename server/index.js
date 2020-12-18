f95d5a565fc0677da0e92ef125208827d6f6325d
Ankit Hans <ankithans1947@gmail.com>
2020-12-18 16:19:45 +0530

    feat: connecting client

--- server/index.js
+++ server/index.js
@@ -13,11 +13,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../", "client", "index.html"));
+});
+
 const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
 
-const rules = [{ value: "coding" }, {}];
+const rules = [{ value: "coding" }];
 
 // Get stream rules
 async function getRules() {
@@ -41,6 +45,7 @@ async function setRules() {
       Authorization: `Bearer ${TOKEN}`,
     },
   });
+
   return response.body;
 }
 
@@ -63,10 +68,11 @@ async function deleteRules(rules) {
       Authorization: `Bearer ${TOKEN}`,
     },
   });
+
   return response.body;
 }
 
-function streamTweets() {
+function streamTweets(socket) {
   const stream = needle.get(streamURL, {
     headers: {
       Authorization: `Bearer ${TOKEN}`,
@@ -76,33 +82,29 @@ function streamTweets() {
   stream.on("data", (data) => {
     try {
       const json = JSON.parse(data);
-      console.log(json);
+      socket.emit("tweet", json);
     } catch (error) {}
   });
 }
 
-io.on("connection", () => {
+io.on("connection", async () => {
   console.log("Client connected...");
+  let currentRules;
+
+  try {
+    // Get all stream rules
+    currentRules = await getRules();
+
+    // Delete all stream rules
+    await deleteRules(currentRules);
+
+    // Set rules based on array above
+    await setRules();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  streamTweets(io);
 });
-
-// (async () => {
-//   let currentRules;
-
-//   try {
-//     // Get all stream rules
-//     currentRules = await getRules();
-
-//     // Delete all stream rules
-//     await deleteRules(currentRules);
-
-//     // Set rules based on array above
-//     await setRules();
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-
-//   streamTweets();
-// })();
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
