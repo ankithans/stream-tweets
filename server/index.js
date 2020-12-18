927dce08a32eec0a25b300b8b2f522badfd812ce
Ankit Hans <ankithans1947@gmail.com>
2020-12-18 16:33:34 +0530

    feat: use bootstrap for UI

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const path = require("path");
 const express = require("express");
 const socketIO = require("socket.io");
 const needle = require("needle");
-const { Socket } = require("dgram");
 const config = require("dotenv").config();
 const TOKEN = process.env.TWITTER_BEARER_TOKEN;
 const PORT = process.env.PORT || 3000;
@@ -21,7 +20,7 @@ const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
 
-const rules = [{ value: "coding" }];
+const rules = [{ value: "giveaways" }];
 
 // Get stream rules
 async function getRules() {
